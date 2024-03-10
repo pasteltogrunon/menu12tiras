90df591a60cc6210a03f61e129e4882594fa81b1
Antonio Tigri <entertainmentatv@gmail.com>
2024-03-10 12:46:23 +0100

    Merge conflicts

--- menu12tiras/Assets/GameManager.cs
+++ menu12tiras/Assets/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
                 mobiusStrip = mobiusStripTwisted;
                 break;
         }
-        mobiusStrip.GenerateMobiusStrip(mobiusStrip.material, mobiusStrip.uResolution, mobiusStrip.vResolution);
+        mobiusStrip.GenerateMobiusStrip();
         StartPlayer();
         StartCoroutine(SpawnCoins());
     }
@@ -59,7 +59,7 @@ public class GameManager : MonoBehaviour
 
         player.VPos += Input.GetAxis("Horizontal") * Time.deltaTime * player.hspeed;
         player.VPos = Mathf.Clamp(player.VPos, -1 + EPSILON, 1 - EPSILON);
-        player.UPos += Time.deltaTime * player.fspeed;
+        player.UPos += Time.fixedDeltaTime * player.fspeed;
         UpdatePlayerPosition();
     }
 
@@ -101,5 +101,39 @@ public class GameManager : MonoBehaviour
         Vector3 lookAt = mobiusStrip.LookAt(player.UPos, player.VPos);
         Vector3 offset = player.HPos * (player.Inverted ? -1 : 1) * normal;
         player.GameObject.transform.SetPositionAndRotation(currentPoint + offset, Quaternion.LookRotation(lookAt, normal));
+    }
+
+    //Debugging
+    private void OnDrawGizmos()
+    {
+        if (!debug) return;
+
+        for (float u = 0; u < 2 * Mathf.PI; u += 0.01f)
+        {
+            for (float v = -1; v < 1; v += 0.05f)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(mobiusStrip.GetPosition(u, v), mobiusStrip.GetPosition(u + 0.1f, v));
+            }
+        }
+
+        float ustep = 2 * Mathf.PI / mobiusStrip.uResolution;
+        float hstep = 2f * mobiusStrip.boundHeight / mobiusStrip.hResolution;
+
+        float u_ = 0;
+        float h = -mobiusStrip.boundHeight;
+
+        while (u_ < 2 * Mathf.PI)
+        {
+            while (h < 1)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(mobiusStrip.GetPosition(u_, -1) + h * mobiusStrip.Normal(u_, -1 + EPSILON), 0.01f);
+                Gizmos.DrawSphere(mobiusStrip.GetPosition(u_, 1) + h * mobiusStrip.Normal(u_, 1 - EPSILON), 0.01f);
+                h += hstep;
+            }
+            h = -mobiusStrip.boundHeight;
+            u_ += ustep;
+        }
     }
 }

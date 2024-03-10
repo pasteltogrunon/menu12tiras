6644f8a68ec8bcb5877a6cd4a86d9dcc762cf644
EPG <juanfernandezesteban44@gmail.com>
2024-03-10 15:22:53 +0100

    Player movement

--- menu12tiras/Assets/Player.cs
+++ menu12tiras/Assets/Player.cs
@@ -6,6 +6,8 @@ public class Player
     [Header("Player Settings")]
     [Range(0, 10)] public float fspeed = 1f;
     [Range(0, 10)] public float hspeed = 1f;
+    [Range(0, 15)] public float hacceleration = 10f;
+    [Range(0, 0.3f)] public float hfriction = 0.02f;
 
     [Header("Player Object")]
     private readonly GameObject gameObject;
@@ -15,10 +17,21 @@ public class Player
     private readonly float hpos;
     private readonly bool inverted;
 
+    private float vspeed;
+
     public float UPos { get; set; }
     public float VPos { get; set; }
     public float HPos { get; set; }
     public bool Inverted { get; set; }
     public GameObject GameObject { get; set; }
-    
+
+    public float VSpeed
+    {
+        get => vspeed;
+        set
+        {
+            vspeed = Mathf.Clamp(value, -hspeed * 0.2f * fspeed, hspeed * 0.2f * fspeed);
+        }
+    }
+
 }

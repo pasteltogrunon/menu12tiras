894c32a5d9f4378c9c4f296fd4a25947512ee49c
EPG <juanfernandezesteban44@gmail.com>
2024-03-11 03:40:53 +0100

    UI

--- menu12tiras/Assets/UIManager.cs
+++ menu12tiras/Assets/UIManager.cs
@@ -13,6 +13,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMP_Text Timer;
 
     [SerializeField] private TMP_Text Velocimeter;
+    [SerializeField] private TMP_Text Coins;
+    [SerializeField] private Image die;
 
     private void Update()
     {
@@ -28,6 +30,9 @@ public class UIManager : MonoBehaviour
         Timer.text = string.Format("{0:000}:{1:00}.{2:000}", m, s, (GameManager.instance.TotalTime - t) * 1000);
 
         Velocimeter.text = Mathf.FloorToInt(GameManager.instance.Player.USpeed * 4).ToString();
+        Coins.text = GameManager.instance.Player.Coins.ToString();
+
+        die.fillAmount = Mathf.Clamp01((float)GameManager.instance.Player.Coins/10);
     }
 
 }
